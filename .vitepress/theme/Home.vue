56a9285d90a6488dcda9096369944a5c93c7273c
Evan You <yyx990803@gmail.com>
2021-01-12 17:21:52 -0500

    fix: fix hydration mismatch caused by invalid v-html

--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -15,9 +15,7 @@
               <h2 class="text-2xl leading-8 font-bold tracking-tight">
                 <a class="text-gray-900" :href="href">{{ title }}</a>
               </h2>
-              <div v-if="excerpt" class="prose max-w-none text-gray-500">
-                <p v-html="excerpt"></p>
-              </div>
+              <div v-if="excerpt" class="prose max-w-none text-gray-500" v-html="excerpt"></div>
             </div>
             <div class="text-base leading-6 font-medium">
               <a class="link" aria-label="read more" :href="href">Read more →</a>
