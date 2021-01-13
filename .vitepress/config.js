f6b7acaa634d96acfef0169f6238e2371a8e853d
Evan You <yyx990803@gmail.com>
2021-01-13 12:51:46 -0500

    link to favicon

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,3 +1,4 @@
+// @ts-check
 const { getPosts } = require('./getPosts')
 
 /**
@@ -6,6 +7,16 @@ const { getPosts } = require('./getPosts')
 module.exports = {
   title: 'The Vue Point',
   description: 'The offical blog for the Vue.js project',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
+    ]
+  ],
   customData: {
     posts: getPosts()
   }
