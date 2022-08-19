1950fe8f94ad06476a0825f5b7f6cf28fe0da168
Evan You <yyx990803@gmail.com>
2022-08-19 11:38:59 +0800

    improve 404 style

--- .vitepress/theme/NotFound.vue
+++ .vitepress/theme/NotFound.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>Not Found</h1>
+  <h1 class="text-3xl font-bold">404 Page Not Found</h1>
 </template>
--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-import typography from '@tailwindcss/typography'
-
-export default {
-  content: ['./.vitepress/theme/**/*.vue'],
-  plugins: [typography]
-}
